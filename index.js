f6922bebdbf0d32632869630c83e4eb383dc1fcf
Venika <venika.jarvis@gmail.com>
2021-04-13 08:29:02 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+let totalBatteries = batteryBatches.reduce(
+    (accumulator, currentValue ) => accumulator + currentValue,
+    0
+  )
+
+
+// Example from Lesson
+// const hogwartsHouses = {
+//     "Slytherin": [],
+//     "Gryffindor": [],
+//     "Hufflepuff": [],
+//     "Ravenclaw": []
+//   }
+// incomingStudents.reduce(function(houses, student) {
+//     houses[sortingHat.assign(student)].push(student)}, hogwartsHouses) 
